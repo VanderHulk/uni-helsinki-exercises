b9e44f219d1f00202c8c2b310e730951a73e8cc7
Shaula Miinin <shaula.miinin@gmail.com>
2025-12-26 08:43:51 +0200

    part1/unicafe - Add more statistics total, average, percentage.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,24 +4,40 @@ const Header = ({ title }) => <h2>{title}</h2>
 
 const Button = ({ onClick, text}) => <button onClick = {onClick}>{text}</button>
 
-const Display = ({ text, totalFeedback}) => <p>{text} {totalFeedback}</p>
+const Display = ({ text, result}) => <p>{text} {result}</p>
 
 const App = () => {
   // save clicks of each button on its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [bad, setBad] = useState(0)  
+
+  const collectFeedback = (stateVal, stateUpdater) => {    
+    const count = stateVal + 1    
+    stateUpdater(count)    
+  }
+
+  const getTotal = (valGood, valNeutral, valBad) => valGood + valNeutral + valBad
+  const getAverage = (valGood, valBad, totalCount) => 
+    (totalCount) ? (valGood - valBad) / totalCount : 0
+  const getPositivePercentage = (valGood, totalCount) => 
+    (totalCount) ? (valGood / totalCount) * 100 : 0
+
+  const totalFeedback = getTotal(good, neutral, bad)
 
   return (
     <div>
       <Header title='Give Feedback' />
-      <Button onClick={console.log('Hello')} text='Good' />
-      <Button onClick={console.log('Hello')} text='Neutral' />
-      <Button onClick={console.log('Hello')} text='Bad' />
+      <Button onClick={() => collectFeedback(good, setGood)} text='Good' />
+      <Button onClick={() => collectFeedback(neutral, setNeutral)} text='Neutral' />
+      <Button onClick={() => collectFeedback(bad, setBad)} text='Bad' />
       <Header title='Statistics' />
-      <Display text='Good' totalFeedback={good}/>
-      <Display text='Neutral' totalFeedback={neutral}/>
-      <Display text='Bad' totalFeedback={bad}/>
+      <Display text='Good' result={good} />
+      <Display text='Neutral' result={neutral} />
+      <Display text='Bad' result={bad} />
+      <Display text='Total' result={totalFeedback} />
+      <Display text='Average' result={getAverage(good, bad, totalFeedback)} />
+      <Display text='Positive' result={getPositivePercentage(good, totalFeedback)} />
     </div>
   )
 }
