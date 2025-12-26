12ff541763492861974e8dcf7221a9db7e1d8824
Shaula Miinin <shaula.miinin@gmail.com>
2025-12-26 16:26:09 +0200

    part1/unicafe - Extract total, average, and positive percentage from App to Statistics component.

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,6 +6,20 @@ const Button = ({ onClick, text}) => <button onClick = {onClick}>{text}</button>
 
 const Display = ({ text, result}) => <p>{text} {result}</p>
 
+const Statistics = ({valGood, valNeutral, valBad}) => {
+  const getTotal = valGood + valNeutral + valBad
+  const getAverage = (getTotal) ? (valGood - valBad) / getTotal : 0
+  const getPositivePercentage = (getTotal) ? (valGood / getTotal) * 100 : 0
+
+  return (
+    <>
+      <Display text='Total' result={getTotal} />
+      <Display text='Average' result={getAverage} />
+      <Display text='Positive' result={getPositivePercentage} />
+    </>
+  )
+}
+
 const App = () => {
   // save clicks of each button on its own state
   const [good, setGood] = useState(0)
@@ -15,15 +29,7 @@ const App = () => {
   const collectFeedback = (stateVal, stateUpdater) => {    
     const count = stateVal + 1    
     stateUpdater(count)    
-  }
-
-  const getTotal = (valGood, valNeutral, valBad) => valGood + valNeutral + valBad
-  const getAverage = (valGood, valBad, totalCount) => 
-    (totalCount) ? (valGood - valBad) / totalCount : 0
-  const getPositivePercentage = (valGood, totalCount) => 
-    (totalCount) ? (valGood / totalCount) * 100 : 0
-
-  const totalFeedback = getTotal(good, neutral, bad)
+  }  
 
   return (
     <div>
@@ -35,9 +41,7 @@ const App = () => {
       <Display text='Good' result={good} />
       <Display text='Neutral' result={neutral} />
       <Display text='Bad' result={bad} />
-      <Display text='Total' result={totalFeedback} />
-      <Display text='Average' result={getAverage(good, bad, totalFeedback)} />
-      <Display text='Positive' result={getPositivePercentage(good, totalFeedback)} />
+      <Statistics valGood={good} valNeutral={neutral} valBad={bad}/>
     </div>
   )
 }
