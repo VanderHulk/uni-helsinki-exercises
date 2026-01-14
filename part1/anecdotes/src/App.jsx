45a4068404bc9c9de08bf535868b6d4db2f34a8d
Shaula Miinin <shaula.miinin@gmail.com>
2026-01-14 13:42:17 +0200

    part1/anecdotes
    Changes:
    - move anecdotes array outside of App
    - change randomizeAnecdote to generateRandomNumber, move outside of App
    
    New:
    - generateVotesTally
    - voteAnAnecdote
    - DisplayAnecdoteVotes

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,32 +1,71 @@
 import { useState } from 'react'
 
-const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const anecdotes = [
+  'If it hurts, do it more often.',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+  'The only way to go fast, is to go well.'
+]
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
+// creates an object where the votes will be stored, returns the object
+const generateVotesTally = () => {
+  const votesTally = {}
 
-  const [selected, setSelected] = useState(0)
-
-  const randomizeAnecdote = () => {
-    const randomNumber = Math.floor(Math.random() * anecdotes.length)
-    console.log(randomNumber)
-    setSelected(randomNumber)
+  for (let i = 0; i < anecdotes.length; i++) {
+    votesTally[i] = 0;
   }
 
+  return votesTally
+}
+
+// generates a random number and use it to choose an anecdote from the array
+const generateRandomNumber = () => {
+  const randomNumber = Math.floor(Math.random() * anecdotes.length)
+  console.log('Generated Number: ', randomNumber)
+  return randomNumber
+}
+
+
+// COMPONENTS:
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
+const DisplayAnecdoteVotes = ({anecdote, voteCount}) => {
   return (
-    <div>
-      <div>{anecdotes[selected]}</div>
+    <>
+      <div>{anecdote}</div>
       <br />
-      <Button onClick={() => randomizeAnecdote()} text='Next Anecdote' />
+      <div>{`has ${voteCount} votes`}</div>      
+    </>
+  )
+}
+
+const App = () => {  
+
+  const [selected, setSelected] = useState(0)
+  const [voteCount, setVoteCount] = useState(generateVotesTally())
+  
+  const voteAnAnecdote = () => {    
+    const votesCopy = { ...voteCount } // make a shallow copy to update state without mutating the original object
+
+    votesCopy[selected] += 1;
+    // console.log(anecdotes[selected], votesCopy[selected])
+    setVoteCount(votesCopy)
+  }
+
+  // console.log(voteCount)
+   
+  return (
+    <div>      
+      <DisplayAnecdoteVotes anecdote={anecdotes[selected]} voteCount={voteCount[selected]} />      
+      <br />
+      <Button onClick={() => voteAnAnecdote(selected)} text='Vote' />
+      {/* pick a new random anecdote */}
+      <Button onClick={() => setSelected(generateRandomNumber())} text='Next Anecdote' />
     </div>
   )
 }
