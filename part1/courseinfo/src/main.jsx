dc41bd3dcc22dd8b68f6a448c89a4ef18714017f
Shaula Miinin <shaula.miinin@gmail.com>
2026-01-20 11:10:19 +0200

    part1/courseinfo
    - add comments in main.jsx to explain ReactDOM and App
    - update README.md with final notes on component structure and props

--- part1/courseinfo/src/main.jsx
+++ part1/courseinfo/src/main.jsx
@@ -2,3 +2,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+//  ReactDOM.createRoot, creates a React root, which is the entry point for a React application
+//  document.getElementById('root'), finds the html element in index.html that has id of root where React can attach itself, also tells React to take control of this DOM element
+//  .render(<App />) , tells react to render the App component inside that root
+//  from App, React will then render all the child components
