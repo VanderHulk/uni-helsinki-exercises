25f49544745d4ac79152e5c0248309973d72939a
Shaula Miinin <shaula.miinin@gmail.com>
2026-02-04 13:34:15 +0200

    First commit of part2/phonebook - add form handling and person list rendering

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {
+       id: '1',
+      name: 'Arto Hellas'      
+    }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addContact = (event) => {
+    event.preventDefault()
+    const contact = {
+      id: String(persons.length + 1),
+      name: newName  
+    }    
+    setPersons(persons.concat(contact))
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addContact}>
+        <div>
+          name: <input
+            value = { newName }
+            onChange = {(e) => setNewName(e.target.value)}
+          />          
+        </div>
+        <div>
+          <button type='submit'>add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => 
+            <li key={ person.id }>
+              { person.name }
+            </li>
+        )}
+      </ul>
+    </div>
+  )
+}
+
+export default App
